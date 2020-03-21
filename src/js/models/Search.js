4f48eb49fe8ef363d228072b767d15f006c15ce2
Swetha Ganapathy subramanian <swethaganapathy2@gmail.com>
2020-03-20 21:14:59 -0700

    webpack problem fixed

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,11 +1,11 @@
-
+const key = "48c67776367d49539794c154f0c8bc36";
 export default class Search {
     constructor(query){
         this.query = query;
     }
 
 async getRecipes() {
-    const key = process.env.API_KEY;
+  
     try {
         const number = 30;
         const url = `https://api.spoonacular.com/recipes/search?apiKey=${key}&query=${this.query}&number=${number}`
